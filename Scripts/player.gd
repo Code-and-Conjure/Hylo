bbb599de622650b0d0d43803b4be83e1bc5944f6
Nick Dolan <nicholas.dolan1@gmail.com>
2024-07-19 14:35:37 -0500

    Initial Project

---
+++ Scripts/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+const SPEED = 20000.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	var verticalDirection = Input.get_axis("ui_up", "ui_down")
+	
+	if direction:
+		velocity.x = direction
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED);
+		
+	if verticalDirection:
+		velocity.y = verticalDirection
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED);
+	
+	velocity = velocity.normalized() * SPEED * delta
+		
+
+	move_and_slide()

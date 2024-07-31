1795adfc76c7fd467b8e4caeae3582d6adfe8759
Nicholas Dolan <nicholas.dolan1@gmail.com>
2024-07-31 03:28:25 -0500

    Text sells (#27)
    * added text to remaining levels; needs fixin'
    
    * Fixed text properties not showing
    
    * Added text message capabilities
    
    * I don't know what I did here
    
    ---------
    
    Co-authored-by: anarchyjones <112559334+anarchyjones@users.noreply.github.com>

--- Scripts/air_level.gd
+++ Scripts/air_level.gd
@@ -3,10 +3,17 @@ extends Node2D
 
 @onready var area_2d = $Area2D
 @onready var air_boss:AirBoss = $AirBoss
+@onready var message: RichTextLabel = %Message
+
+func _ready():
+	Events.display_text.connect(set_message_text)
 
 func _on_area_2d_body_entered(_dbody: Node2D):
 	air_boss.recallThings()
 
 func _on_air_boss_die() -> void:
 	$BargainingMask.visible = true
-	
+
+
+func set_message_text(text: String) -> void:
+	message.text = text

af5695209226eaee87e5d5a271398dd98ac41114
Nick Dolan <nicholas.dolan1@gmail.com>
2024-07-31 03:32:11 -0500

    Added build export

--- Scenes/yellow_earth_denial.gd
+++ Scenes/yellow_earth_denial.gd
@@ -8,7 +8,6 @@ func _ready():
 func set_message_text(text: String) -> void:
 	message.text = text
 
-
 func _on_killzone_body_entered(body: Node2D) -> void:
 	var player = body as PlatformingPlayer
 	if not player:

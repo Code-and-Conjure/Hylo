e4fc5056065484804d647d998fabcd898bf3a08b
Nick Dolan <nicholas.dolan1@gmail.com>
2024-07-31 02:54:49 -0500

    Added earth enemy node

--- Scripts/earth_enemy.gd
+++ Scripts/earth_enemy.gd
@@ -8,7 +8,9 @@ var velocity: Vector2 = Vector2.ZERO
 var is_right: bool = true
 
 func _physics_process(delta: float) -> void:
-	if is_right:
+	if target is PlatformingPlayer: # If you can sneak while in the zone `and target.sneaking_slowdown == 1.0`
+		velocity.x = speed * delta * sign(target.position.x - position.x)
+	elif is_right:
 		velocity.x = speed * delta
 	else :
 		velocity.x = -speed * delta
@@ -18,5 +20,13 @@ func _physics_process(delta: float) -> void:
 		is_right = not is_right
 
 func _on_detection_body_entered(body: Node2D) -> void:
-	if body is PlatformingPlayer:
+	if body is PlatformingPlayer and body.sneaking_slowdown == 1.0:
 		target = body
+
+func _on_detection_body_exited(body: Node2D) -> void:
+	if body is PlatformingPlayer:
+		target = null
+
+func _on_attack_body_entered(body: Node2D) -> void:
+	if body is PlatformingPlayer and body.sneaking_slowdown == 1.0:
+		body.damage(15)
